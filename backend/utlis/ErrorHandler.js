7d2c4160039c887d34dab169a3bbc740ea1834a2
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-04-06 22:52:59 +0530

    added database setup , added middleware Error, added ErrorHandler of utils and added starting backend setup

---
+++ backend/utlis/ErrorHandler.js
@@ -0,0 +1,11 @@
+class ErrorHandler extends Error{
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode = statusCode
+
+        Error.captureStackTrace(this,this.constructor);   //captureStackTrace used to know the place where the error is occured
+
+    }
+    
+}
+module.exports = ErrorHandler;
