4db5f2eaa6ef3f06fecf21687692cd273412f279
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-04-06 22:11:04 +0530

    added signUp UI

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+
+//Config
+if(process.env.NODE_ENV !== "PRODUCTION"){
+    require("dotenv").config({
+        path:"backend/config/.env"
+    })
+}
+
+
+module.exports = app;
