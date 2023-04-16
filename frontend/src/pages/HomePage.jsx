6bca26aafb7fe992ed63f1c07801332f1a668445
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-04-16 21:52:36 +0530

    added hero section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from '../components/Layout/Header.jsx';
+import Hero from "../components/Route/Hero/Hero.jsx";
+import Categories from "../components/Route/Categories/Categories.jsx";
 
 const HomePage = () => {
   return (
     <div>
+      {/* activeheading is used to highligh active navbar list item */}
         <Header activeHeading={1}/>
+        <Hero />  
+        <Categories />
     </div>
   )
 }
