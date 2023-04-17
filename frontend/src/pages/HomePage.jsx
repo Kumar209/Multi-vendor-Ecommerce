0f72dbcad12490b069a9f13fdecc22c82263f5d6
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-04-17 21:48:01 +0530

    added BestDeals section along with ProductCard

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Layout/Header.jsx';
 import Hero from "../components/Route/Hero/Hero.jsx";
 import Categories from "../components/Route/Categories/Categories.jsx";
+import BestDeals from "../components/Route/BestDeals/BestDeals.jsx";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
         <Header activeHeading={1}/>
         <Hero />  
         <Categories />
+        <BestDeals />
     </div>
   )
 }
