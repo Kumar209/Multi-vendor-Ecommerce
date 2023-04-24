7883f93cddec486b60f8778621ea0d2d92b13a23
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-04-24 17:51:34 +0530

    added refunds section in Profile page

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -89,6 +89,13 @@ const ProfileContent = ({ active }) => {
           <AllOrders />
         </div>
       )}
+
+      {/* Refund page  */}
+      {active === 3 && (
+        <div>
+          <AllRefundOrders />
+        </div>
+      )}
     </div>
   );
 };
@@ -171,6 +178,87 @@ const AllOrders  = () => {
   return (
     <div className="pl-8 pt-1">
       <DataGrid rows={row} columns={columns} pageSize={10} disableSelectionOnClick autoHeight />
+    </div>
+  )
+}
+
+const AllRefundOrders= () => {
+  const orders = [
+    {
+      _id: "7463HVBHADSKJFK5563",
+      orderItems: [
+        {
+          name: "Iphone 14 pro max",
+        },
+      ],
+      totalPrice : 120,
+      orderStatus: "Processing",
+    },
+  ];
+
+  const columns = [
+    { field:"id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+    {
+      field : "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "status") === "Delivered" ? "greenColor" : "redColor";
+      },
+    },
+
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field : "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field : " ",
+      flex : 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+          <Link to={`/order/${params.id}`}>
+            <Button>
+              <AiOutlineArrowRight size={20} />
+            </Button>
+          </Link>
+          </>
+        )
+      }
+    }
+  ]
+
+  const row = [];
+
+  orders && orders.forEach((item) => {
+    row.push({
+      id: item._id,
+      itemsQty : item.orderItems.length,
+      total: "US$" + item.totalPrice,
+      status: item.orderStatus,
+    });
+  });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid rows={row} columns={columns} pageSize={10} autoHeight disableSelectionOnClick />
     </div>
   )
 }
